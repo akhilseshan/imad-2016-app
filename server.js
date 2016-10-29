3708eaffbae948aac6a6dc286ce06112e0fc1172
akhilseshan <akhilses@gmail.com>
2016-10-29 12:16:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/green-grass-drew-morning-pure-macro-water-herbs-wallpaper.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'green-grass-drew-morning-pure-macro-water-herbs-wallpaper.png'));
 });
 
